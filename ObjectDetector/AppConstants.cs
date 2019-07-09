ad25aa4d18a9c7b7a057a36e4b6affc04d2b1e2d
Jim Bennett <jim.bennett@microsoft.com>
2019-07-09 13:01:18 +0100

    Update AppConstants.cs

--- ObjectDetector/AppConstants.cs
+++ ObjectDetector/AppConstants.cs
@@ -2,7 +2,14 @@
 {
     public static class AppConstants
     {
+#error Add these entries!
         // Please enter your key from appcenter.ms
         public const string AppCenterKey = "ios=";
+
+        // Head to https://aka.ms/AA5jkiu
+        // Create a new computer vision resource
+        // Add the API key and endpoint here
+        public const string OcrKey = "";
+        public const string OcrEndpoint = "";
     }
 }
